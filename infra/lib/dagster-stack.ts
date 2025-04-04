f1d497bfe09f77c10eedf7b331cca2b96a24ef18
Cadell Christo <cadell.c@montu.com.au>
2025-04-04 11:59:09 +0800

    setup daemon and repository, connected with service connect

--- infra/lib/dagster-stack.ts
+++ infra/lib/dagster-stack.ts
@@ -1,21 +1,24 @@
-import { RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {
   InstanceClass,
   InstanceSize,
   InstanceType,
+  Peer,
   Port,
   SecurityGroup,
   SubnetType,
   Vpc,
 } from 'aws-cdk-lib/aws-ec2';
 import {
+  AppProtocol,
   AwsLogDriverMode,
   Cluster,
   ContainerImage,
   FargateService,
   FargateTaskDefinition,
   LogDrivers,
+  Protocol,
   Secret,
 } from 'aws-cdk-lib/aws-ecs';
 import { EcrStack } from './ecr-stack';
@@ -28,6 +31,7 @@ import {
   DatabaseInstanceEngine,
   DatabaseSecret,
 } from 'aws-cdk-lib/aws-rds';
+import { NamespaceType } from 'aws-cdk-lib/aws-servicediscovery';
 
 // import { MontuSharedVpc } from '@montugroup/infra';
 
@@ -70,42 +74,19 @@ export class DagsterStack extends Stack {
 
     const database = new DatabaseInstance(this, 'DagsterDatabase', {
       engine: DatabaseInstanceEngine.POSTGRES,
-      databaseName: 'dagster',
-      instanceIdentifier: 'dagster',
       instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
-      allocatedStorage: 20,
-      credentials: Credentials.fromGeneratedSecret('dagster'),
+      instanceIdentifier: 'dagster',
       vpc,
       vpcSubnets: {
         subnetType: SubnetType.PRIVATE_ISOLATED,
       },
       securityGroups: [databaseSecurityGroup],
+      databaseName: 'dagster',
+      credentials: Credentials.fromGeneratedSecret('dagster'),
+      allocatedStorage: 20,
       storageEncrypted: true,
       removalPolicy: RemovalPolicy.DESTROY,
     });
-
-    const cluster = new Cluster(this, 'DagsterCluster', {
-      vpc,
-      clusterName: 'dagster-cluster',
-    });
-
-    const taskDefinition = new FargateTaskDefinition(this, 'DagsterTaskDefinition', {
-      cpu: 1024,
-      memoryLimitMiB: 2048,
-    });
-
-    // const { repository } = ecrStack;
-
-    // repository.grantPull(taskDefinition.obtainExecutionRole());
-
-    const image: ContainerImage = ContainerImage.fromDockerImageAsset(
-      new DockerImageAsset(this, 'dagster-image-asset', {
-        directory: path.join(process.cwd(), '..'),
-        file: 'dagster/dagster.Dockerfile',
-        platform: Platform.LINUX_AMD64,
-      }),
-    );
-    // ContainerImage.fromEcrRepository(repository, `sha-${123456}`);
 
     const dagsterSecrets = {
       DAGSTER_POSTGRES_USER: Secret.fromSecretsManager(database.secret!, 'username'),
@@ -115,14 +96,46 @@ export class DagsterStack extends Stack {
       DAGSTER_POSTGRES_PORT: Secret.fromSecretsManager(database.secret!, 'port'),
     };
 
-    taskDefinition.addContainer('DagsterContainerImage', {
-      image,
+    const cluster = new Cluster(this, 'DagsterCluster', {
+      clusterName: 'dagster-cluster',
+      vpc,
+    });
+    const namespace = cluster.addDefaultCloudMapNamespace({
+      name: 'dagster.private',
+      useForServiceConnect: true,
+      type: NamespaceType.DNS_PRIVATE,
+      vpc,
+    });
+    // const { repository } = ecrStack;
+
+    const webserverTaskDefinition = new FargateTaskDefinition(
+      this,
+      'DagsterWebserverTaskDefinition',
+      {
+        cpu: 1024,
+        memoryLimitMiB: 2048,
+      },
+    );
+
+    // repository.grantPull(taskDefinition.obtainExecutionRole());
+
+    const dagsterImage: ContainerImage = ContainerImage.fromDockerImageAsset(
+      new DockerImageAsset(this, 'dagster-image-asset', {
+        directory: path.join(process.cwd(), '..'),
+        file: 'dagster/dagster.Dockerfile',
+        platform: Platform.LINUX_AMD64,
+      }),
+    );
+    // ContainerImage.fromEcrRepository(repository, `sha-${123456}`);
+
+    webserverTaskDefinition.addContainer('DagsterWebserver', {
+      image: dagsterImage,
       entryPoint: ['dagster-webserver', '-h', '0.0.0.0', '-p', '3000', '-w', 'workspace.yaml'],
       logging: LogDrivers.awsLogs({
-        streamPrefix: 'dagster',
+        streamPrefix: 'dagster-webserver',
         mode: AwsLogDriverMode.NON_BLOCKING,
-        logGroup: new LogGroup(this, 'DagsterLogGroup', {
-          logGroupName: 'dagster',
+        logGroup: new LogGroup(this, 'DagsterWebserverLogGroup', {
+          logGroupName: 'dagster-webserver',
           retention: RetentionDays.TWO_WEEKS,
           removalPolicy: RemovalPolicy.DESTROY,
         }),
@@ -133,26 +146,166 @@ export class DagsterStack extends Stack {
       },
     });
 
-    const dagsterSecurityGroup = new SecurityGroup(this, 'DagsterSecurityGroup', {
+    const dagsterWebserverSecurityGroup = new SecurityGroup(this, 'DagsterWebserverSecurityGroup', {
       vpc,
-      description: 'Dagster',
+      description: 'Dagster Webserver',
       allowAllOutbound: true,
     });
-    databaseSecurityGroup.addIngressRule(dagsterSecurityGroup, Port.POSTGRES);
+    databaseSecurityGroup.addIngressRule(dagsterWebserverSecurityGroup, Port.POSTGRES);
 
-    new FargateService(this, 'DagsterFargateService', {
+    new FargateService(this, 'DagsterWebserverFargateService', {
       cluster,
-      taskDefinition,
+      taskDefinition: webserverTaskDefinition,
       vpcSubnets: {
         subnetType: SubnetType.PRIVATE_WITH_EGRESS,
       },
-      securityGroups: [dagsterSecurityGroup],
-      serviceName: 'dagster-service',
+      securityGroups: [dagsterWebserverSecurityGroup],
+      serviceName: 'webserver',
       desiredCount: 1,
       minHealthyPercent: 0,
       circuitBreaker: {
         enable: true,
         rollback: true,
+      },
+      serviceConnectConfiguration: {
+        namespace: namespace.namespaceArn,
+      },
+    });
+
+    const daemonTaskDefinition = new FargateTaskDefinition(this, 'DagsterDaemonTaskDefinition', {
+      cpu: 1024,
+      memoryLimitMiB: 2048,
+    });
+
+    daemonTaskDefinition.addContainer('DagsterDaemon', {
+      image: dagsterImage,
+      entryPoint: ['dagster-daemon', 'run'],
+      logging: LogDrivers.awsLogs({
+        streamPrefix: 'dagster-daemon',
+        mode: AwsLogDriverMode.NON_BLOCKING,
+        logGroup: new LogGroup(this, 'DagsterDaemonLogGroup', {
+          logGroupName: 'dagster-daemon',
+          retention: RetentionDays.TWO_WEEKS,
+          removalPolicy: RemovalPolicy.DESTROY,
+        }),
+      }),
+      environment: {},
+      secrets: {
+        ...dagsterSecrets,
+      },
+    });
+
+    const dagsterDaemonSecurityGroup = new SecurityGroup(this, 'DagsterSecurityGroup', {
+      vpc,
+      description: 'Dagster Daemon',
+      allowAllOutbound: true,
+    });
+    databaseSecurityGroup.addIngressRule(dagsterDaemonSecurityGroup, Port.POSTGRES);
+
+    new FargateService(this, 'DagsterDaemonFargateService', {
+      cluster,
+      taskDefinition: daemonTaskDefinition,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroups: [dagsterDaemonSecurityGroup],
+      serviceName: 'daemon',
+      desiredCount: 1,
+      minHealthyPercent: 0,
+      circuitBreaker: {
+        enable: true,
+        rollback: true,
+      },
+    });
+
+    const repositoryTaskDefinition = new FargateTaskDefinition(
+      this,
+      'DagsterRepositoryTaskDefinition',
+      {
+        cpu: 1024,
+        memoryLimitMiB: 2048,
+      },
+    );
+
+    // repository.grantPull(taskDefinition.obtainExecutionRole());
+
+    const repositoryImage: ContainerImage = ContainerImage.fromDockerImageAsset(
+      new DockerImageAsset(this, 'dagster-repository-image-asset', {
+        directory: path.join(process.cwd(), '..'),
+        file: 'dagster/repository.Dockerfile',
+        platform: Platform.LINUX_AMD64,
+      }),
+    );
+    // ContainerImage.fromEcrRepository(repository, `sha-${123456}`);
+
+    const repositoryServiceName = 'repository';
+    const repositoryPort = 4000;
+
+    repositoryTaskDefinition
+      .addContainer('DagsterRepository', {
+        image: repositoryImage,
+        logging: LogDrivers.awsLogs({
+          streamPrefix: 'dagster-repository',
+          mode: AwsLogDriverMode.NON_BLOCKING,
+          logGroup: new LogGroup(this, 'DagsterRepositoryLogGroup', {
+            logGroupName: 'dagster-repository',
+            retention: RetentionDays.TWO_WEEKS,
+            removalPolicy: RemovalPolicy.DESTROY,
+          }),
+        }),
+        environment: {},
+        secrets: {
+          ...dagsterSecrets,
+        },
+      })
+      .addPortMappings({
+        name: repositoryServiceName,
+        containerPort: repositoryPort,
+        appProtocol: AppProtocol.grpc,
+        protocol: Protocol.TCP,
+      });
+
+    const dagsterRepositorySecurityGroup = new SecurityGroup(
+      this,
+      'DagsterRepositorySecurityGroup',
+      {
+        vpc,
+        description: 'Dagster Repository',
+        allowAllOutbound: true,
+      },
+    );
+    dagsterRepositorySecurityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(repositoryPort),
+      'repository inbound',
+      false,
+    );
+    databaseSecurityGroup.addIngressRule(dagsterRepositorySecurityGroup, Port.POSTGRES);
+
+    new FargateService(this, 'DagsterRepositoryFargateService', {
+      cluster,
+      taskDefinition: repositoryTaskDefinition,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroups: [dagsterRepositorySecurityGroup],
+      serviceName: repositoryServiceName,
+      desiredCount: 1,
+      minHealthyPercent: 0,
+      circuitBreaker: {
+        enable: true,
+        rollback: true,
+      },
+      serviceConnectConfiguration: {
+        namespace: namespace.namespaceArn,
+        services: [
+          {
+            portMappingName: repositoryServiceName,
+            dnsName: repositoryServiceName,
+            port: repositoryPort,
+            perRequestTimeout: Duration.seconds(10),
+          },
+        ],
       },
     });
   }
